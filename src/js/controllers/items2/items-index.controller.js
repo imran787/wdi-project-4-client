ae1303fcc9de5ffb42061ab6c7173c4f797f7670
imran787 <imran.mughal87@gmail.com>
2017-06-12 15:48:51 +0100

    added limiting of characters on index controller and added new filter to limit characters

--- src/js/controllers/items2/items-index.controller.js
+++ src/js/controllers/items2/items-index.controller.js
@@ -7,31 +7,32 @@ ItemsIndexCtrl.$inject = ['Item', 'CurrentUserService', 'API', '$http', '$rootSc
 function ItemsIndexCtrl(Item, CurrentUserService, API, $http, $rootScope, Request){
   const vm       = this;
   vm.updateOffer = updateOffer;
+  // vm.itemText;
 
   Item
-    .query()
-    .$promise
-    .then(items => {
-      vm.filteredItems = items.filter(item => {
-        if (item.seller.id !== CurrentUserService.currentUser.id && item.requests.find(request => request.status === 'accepted') === undefined) return item;
-      });
-
-      vm.offers      = CurrentUserService.currentUser.sent_requests;
-      vm.pendingRequests = CurrentUserService.currentUser.received_requests.filter(request => {
-        if (request.status === 'pending') return request;
-      });
+  .query()
+  .$promise
+  .then(items => {
+    vm.filteredItems = items.filter(item => {
+      if (item.seller.id !== CurrentUserService.currentUser.id && item.requests.find(request => request.status === 'accepted') === undefined) return item;
     });
+
+    vm.offers      = CurrentUserService.currentUser.sent_requests;
+    vm.pendingRequests = CurrentUserService.currentUser.received_requests.filter(request => {
+      if (request.status === 'pending') return request;
+    });
+  });
 
   function updateOffer(request, action) {
     request.status = action;
 
     Request
-      .update({ id: request.id}, request)
-      .$promise
-      .then(()=>{
-        const index = vm.pendingRequests.indexOf(request);
-        vm.pendingRequests.splice(index, 1);
-        console.log('Request has been successfully updated');
-      });
+    .update({ id: request.id}, request)
+    .$promise
+    .then(()=>{
+      const index = vm.pendingRequests.indexOf(request);
+      vm.pendingRequests.splice(index, 1);
+      console.log('Request has been successfully updated');
+    });
   }
 }
--- src/js/controllers/items2/items-show.controller.js
+++ src/js/controllers/items2/items-show.controller.js
@@ -5,76 +5,27 @@ angular
 ItemsShowCtrl.$inject = ['$stateParams', 'Item', '$state', 'CurrentUserService', 'Request'];
 
 function ItemsShowCtrl($stateParams, Item, $state, CurrentUserService, Request){
-  const vm = this;
+  const vm         = this;
+  vm.showRequest   = false;
+  vm.user          = CurrentUserService.currentUser;
+  vm.item          = Item.get($stateParams);
+  vm.createRequest = createRequest;
+  vm.itemsDelete   = itemsDelete;
 
-  vm.user = CurrentUserService.currentUser;
-  // console.log(vm.user);
-  vm.item = Item.get($stateParams);
-
-  vm.request = () => {
-    vm.request = {};
-    // console.log(vm.item);
-    // if ((vm.task.find(x => x.user._id === vm.user._id)) || (vm.task.createdBy === vm.user._id)) {
-    //   return console.log('Sorry you can\'t request this item - you either created it, or have already requested it.');
-    // } else  {
-    //   return console.log('request success');
-    // }
+  function createRequest() {
     vm.request.buyer_id = CurrentUserService.currentUser.id;
     vm.request.seller_id = vm.item.seller.id;
     vm.request.status = 'pending';
     vm.request.item_id = vm.item.id;
-    console.log(vm.request);
-    console.log(`user ${vm.request.buyer_id} has made a request!`);
-    // console.log(vm.request.seller.id);
 
-    ///if current user who has request this is the current user then cant make request
-    if(vm.request.buyer_id === vm.request.seller_id){
-      console.log('no no! you\'ve been a bad bad boy!!');
-    } else{
-      console.log('what\'s youre name, who\'s youre daddy!....is he rich like me??');
+    if(vm.request.buyer_id !== vm.request.seller_id){
       Request
       .save(vm.request)
       .$promise
       .then(()=>{
         $state.go('itemsIndex');
-        // console.log(vm.request);
       });
-
     }
-
-
-
-
-
-
-
-    // if (vm.item.assignedTo.find(x => x._id === user._id)) {
-    //
-    //
-    //     return console.log('Sorry that user is already assigned to this task.');
-    //   }
-    //   vm.task.assignedTo.push(user._id);
-    //   if (!vm.task.charity) vm.task.charity = charity._id;
-    //   vm.task.requestedBy.splice($index, 1);
-    //   Task
-    //     .update({ id: $stateParams.id }, vm.task)
-    //     .$promise
-    //     .then(() => {
-    //       vm.modalText = 'User successfully assigned.';
-    //       vm.showModal();
-    //       vm.task = Task.get($stateParams);
-    //     });
-    // };
-  };
-
-  // delete in show?
-  vm.delete = itemsDelete;
-
-  itemsShow();
-
-  function itemsShow(){
-    vm.item = Item.get($stateParams);
-
   }
 
   function itemsDelete(){
@@ -86,53 +37,3 @@ function ItemsShowCtrl($stateParams, Item, $state, CurrentUserService, Request){
     });
   }
 }
-
-
-
-
-//seller accepts or rejects request made on item?
-
-
-// vm.assign = (user, charity, $index) => {
-//   if (vm.task.assignedTo.find(x => x._id === user._id)) {
-//     vm.modalText = 'Sorry that user is already assign to this task.';
-//     vm.showModal();
-//     return console.log('Sorry that user is already assigned to this task.');
-//   }
-//   vm.task.assignedTo.push(user._id);
-//   if (!vm.task.charity) vm.task.charity = charity._id;
-//   vm.task.requestedBy.splice($index, 1);
-//   Task
-//     .update({ id: $stateParams.id }, vm.task)
-//     .$promise
-//     .then(() => {
-//       vm.modalText = 'User successfully assigned.';
-//       vm.showModal();
-//       vm.task = Task.get($stateParams);
-//     });
-// };
-//
-// vm.showModal = () => {
-//   vm.requestModal.style.display = 'block';
-// };
-// window.onclick = function(event) {
-//   if (event.target == vm.requestModal) {
-//     vm.requestModal.style.display = 'none';
-//   }
-// };
-//
-// vm.closeModal = () => {
-//   vm.requestModal.style.display = 'none';
-// };
-//
-// vm.requestModal = document.getElementById('requestModal');
-//
-// vm.reject = (user, charity, $index) => {
-//   vm.task.requestedBy.splice($index, 1);
-//   Task
-//     .update({ id: $stateParams.id }, vm.task)
-//     .$promise
-//     .then(() => {
-//       vm.task = Task.get($stateParams);
-//     });
-// };
